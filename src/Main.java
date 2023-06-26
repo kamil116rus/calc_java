765b1dab9ec440c860dc78edc245eca0d42602ee
вkamil116rus <kh8@rambler.ru>
2023-06-26 17:44:34 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -2,41 +2,41 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-  public static void main(String[] args) throws Exception {
-    System.out.println("Введите выражение:");
-    Scanner scanner = new Scanner(System.in);
-    String input = scanner.nextLine();
-    System.out.println(calc(input));
-  }
-  public static String calc(String input) throws Exception {
-    int res = 0;
-    int operand1, operand2;
-    String[]tokens = input.split("[/*-+\\s]");
-    String operator = getoperator(input);
-    if (tokens.length != 2 || operator == null)
-      throw new IOException (" - формат математической операции не удовлетворяет заданию - два операнда и один оператор");
-    else if(roman.itsRoman(tokens[0]) && roman.itsRoman(tokens[1])) {
-        operand1 = roman.getIntFromRoman(tokens[0]);
-        operand2 = roman.getIntFromRoman(tokens[1]);
-    } else if()
-
-    // System.out.println(operand2);
-    return Integer.toString(switch (operator)) {
-      case 42:
-        res = operand1 * operand2;
-        break;
-      case 43:
-        res = operand1 + operand2;
-        break;
-      case 45:
-        res = operand1 - operand2;
-        break;
-      case 47:
-        res = operand1 / operand2;
-        break;
+    public static void main(String[] args) throws Exception {
+        System.out.println("Введите выражение:");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        System.out.println(calc(input));
     }
-  }
-//    static String[] validation(String input) throws Exception {
+    public static String calc(String input) throws Exception {
+        int res = 0;
+        int operand1, operand2;
+        String[]tokens = input.split("[/*-+\\s]");
+        String operator = getoperator(input);
+        if (tokens.length != 2 || operator == null)
+            throw new IOException (" - формат математической операции не удовлетворяет заданию - два операнда и один оператор");
+        else if(roman.itsRoman(tokens[0]) && roman.itsRoman(tokens[1])) {
+            operand1 = roman.getIntFromRoman(tokens[0]);
+            operand2 = roman.getIntFromRoman(tokens[1]);
+        } else if()
+
+            // System.out.println(operand2);
+            return Integer.toString(switch (operator)) {
+            case 42:
+                res = operand1 * operand2;
+                break;
+            case 43:
+                res = operand1 + operand2;
+                break;
+            case 45:
+                res = operand1 - operand2;
+                break;
+            case 47:
+                res = operand1 / operand2;
+                break;
+        }
+    }
+    //    static String[] validation(String input) throws Exception {
 //    int operand1, operand2;
 //      String[] tokens = input.split("[/*-+\\s]");
 //      String operator = getoperator(input);
@@ -46,15 +46,15 @@ public class Main {
 //
 //      return tokens;
 //    }
-       static String getoperator(String  input) {
-          String operator;
-          if(input.contains("*")) operator = "*";
-          else if(input.contains("/")) operator = "/";
-          else if (input.contains("-")) operator = "-";
-          else if (input.contains("+")) operator = "+";
-          else  operator = null;
-          return operator;
-      }
+    static String getoperator(String  input) {
+        String operator;
+        if(input.contains("*")) operator = "*";
+        else if(input.contains("/")) operator = "/";
+        else if (input.contains("-")) operator = "-";
+        else if (input.contains("+")) operator = "+";
+        else  operator = null;
+        return operator;
+    }
 
 
 
