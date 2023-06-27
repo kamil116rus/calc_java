015a84d1abd190d64a46f7b122acb7a8f87a7905
вkamil116rus <kh8@rambler.ru>
2023-06-27 18:24:26 +0300

    минимально рабочая версия

--- src/Main.java
+++ src/Main.java
@@ -2,60 +2,78 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Введите выражение:");
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        System.out.println(calc(input));
+  public static void main(String[] args) throws Exception {
+   // System.out.println(Roman.stringFromInt(85));
+    System.out.println("Введите выражение:");
+    Scanner scanner = new Scanner(System.in);
+    String input = scanner.nextLine();
+    System.out.println(calc(input));
+  }
+  public static String calc(String input) throws Exception {
+    int res = 0;
+    int operand1= 0, operand2 = 0;
+    String[] tokens = input.split("[/*-+\\s]+");
+    String operator = getoperator(input);
+    if (tokens.length != 2 || operator == null)
+      throw new IOException(
+          " - формат математической операции не удовлетворяет заданию - два операнда и один оператор");
+    else if (Roman.itsRoman(tokens[0]) && Roman.itsRoman(tokens[1])) {
+      operand1 = Roman.getIntFromRoman(tokens[0]);
+      operand2 = Roman.getIntFromRoman(tokens[1]);
     }
-    public static String calc(String input) throws Exception {
-        int res = 0;
-        int operand1, operand2;
-        String[]tokens = input.split("[/*-+\\s]");
-        String operator = getoperator(input);
-        if (tokens.length != 2 || operator == null)
-            throw new IOException (" - формат математической операции не удовлетворяет заданию - два операнда и один оператор");
-        else if(roman.itsRoman(tokens[0]) && roman.itsRoman(tokens[1])) {
-            operand1 = roman.getIntFromRoman(tokens[0]);
-            operand2 = roman.getIntFromRoman(tokens[1]);
-        } else if()
-
-            // System.out.println(operand2);
-            return Integer.toString(switch (operator)) {
-            case 42:
-                res = operand1 * operand2;
-                break;
-            case 43:
-                res = operand1 + operand2;
-                break;
-            case 45:
-                res = operand1 - operand2;
-                break;
-            case 47:
-                res = operand1 / operand2;
-                break;
-        }
-    }
-    //    static String[] validation(String input) throws Exception {
-//    int operand1, operand2;
-//      String[] tokens = input.split("[/*-+\\s]");
-//      String operator = getoperator(input);
-//      if (tokens.length != 2 || operator == null)
-//        throw new IOException (" - формат математической операции не удовлетворяет заданию - два операнда и один оператор");
-//      if(Character.isDigit(tokenw   ))
-//
-//      return tokens;
-//    }
-    static String getoperator(String  input) {
-        String operator;
-        if(input.contains("*")) operator = "*";
-        else if(input.contains("/")) operator = "/";
-        else if (input.contains("-")) operator = "-";
-        else if (input.contains("+")) operator = "+";
-        else  operator = null;
-        return operator;
+    else if (itsDigit(tokens[0]) && itsDigit(tokens[1])) {
+      operand1 = Integer.parseInt(tokens[0]);
+      operand2 = Integer.parseInt((tokens[1]));
     }
 
-
-
+      // System.out.println(operand2);
+     switch (operator) {
+        case "*":
+          res = operand1 * operand2;
+          break;
+        case "+":
+          res = operand1 + operand2;
+          break;
+        case "-":
+          res = operand1 - operand2;
+          break;
+        case "/":
+          res = operand1 / operand2;
+          break;
+      }
+    return Integer.toString(res);
+  }
+  //    static String[] validation(String input) throws Exception {
+  //    int operand1, operand2;
+  //      String[] tokens = input.split("[/*-+\\s]");
+  //      String operator = getoperator(input);
+  //      if (tokens.length != 2 || operator == null)
+  //        throw new IOException (" - формат математической операции не удовлетворяет заданию - два
+  //        операнда и один оператор");
+  //      if(Character.isDigit(tokenw   ))
+  //
+  //      return tokens;
+  //    }
+  static String getoperator(String input) {
+     String operator;
+    if (input.contains("*"))
+      operator = "*";
+    else if (input.contains("/"))
+      operator = "/";
+    else if (input.contains("-"))
+      operator = "-";
+    else if (input.contains("+"))
+      operator = "+";
+    else
+      operator = null;
+    return operator;
+  }
+  static boolean itsDigit(String input) {
+    boolean temp = true;
+    for (int i = 0; i < input.length(); i++) {
+      if (!Character.isDigit(input.toCharArray()[i]))
+        temp = false;
+    }
+    return temp;
+  }
 }
---
+++ src/Roman.java
@@ -0,0 +1,62 @@
+public class Roman {
+  static String[] roman = {"null", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
+  // Map<Integer, String> roman1= new HashMap<>[] ;
+  static boolean itsRoman(String input) {
+    boolean temp = false;
+    for (String str : roman) {
+      if (input.equals(str))
+        temp = true;
+    }
+    return temp;
+  }
+  static int getIntFromRoman(String input) {
+    int temp = 0;
+    for (int i = 1; i < roman.length; i++) {
+      if (input.equals(roman[i]))
+        temp = i;
+    }
+    return temp;
+  }
+  static String stringFromInt(int value) {
+    String temp = new String();
+    while(value > 0) {
+      if (value == 100) {
+        temp += "C";
+        value -= 100;
+      }
+      if (value >= 90) {
+        temp += "XC";
+        value -= 90;
+      }
+      if(value >= 50) {
+        temp += "L";
+        value -= 50;
+      }
+      if(value >=40) {
+        temp += "Xl";
+        value -= 40;
+      }
+      while(value >= 10) {
+        temp += "X";
+        value -= 10;
+      }
+      if(value >=9) {
+        temp+="IX";
+        value -= 9;
+      }
+      if(value >= 5) {
+        temp += "V";
+        value -= 5;
+      }
+      if(value >=4) {
+        temp += "IV";
+        value -= 4;
+      }
+      if(value >= 1) {
+        temp += "I";
+        value -= 1;
+      }
+    }
+    return  temp;
+  }
+}
